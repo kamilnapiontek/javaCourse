ca80fb06c103bf361d5104dc6482350e89344080
p1ontek <kknapiontek@gmail.com>
2022-10-09 23:20:09 +0200

    3.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,9 +9,9 @@ public class Main {
     public static void main(String[] args) {
         TextJustification textJustification = new TextJustification();
         textJustification.writeText(new ArrayList<>(Arrays.asList("This","is","an","example","of","text","justification")),16);
-        System.out.println("***************************");
+        System.out.println("****************************");
         textJustification.writeText(new ArrayList<>(Arrays.asList("What","must","be","acknowledgment","shall","be")),16);
-        System.out.println("***************************");
+        System.out.println("****************************");
         textJustification.writeText(new ArrayList<>(Arrays.asList("Science","is","what","we","understand","well","enough","to",
                 "explain","to","a","computer.","Art","is","everything","else","we","do")),20);
     }
